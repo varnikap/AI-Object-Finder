c1a507221bec7943c2a47ffe9a437e26bda79e9c
varnikap <78656240+varnikap@users.noreply.github.com>
2022-02-26 19:13:40 -0500

    Add files via upload

---
+++ find.js
@@ -0,0 +1,27 @@
+status = "";
+
+function setup()
+{
+    canvas = createCanvas(680,430);
+    canvas.center();
+    video = createVideo(VIDEO);
+    video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+}
+
+function draw()
+{
+    image(video, 0,0,680,430);
+}
